ade9d29e7fc0e93e205e69a65a52690584315db7
davorb00 <dbojic00@fesb.hr>
2017-06-08 22:56:21 +0200

    New scripts added

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public Transform[] patrolPoints;
+	public int speed = 5;
+	private int currentPoint;
+
+	// Use this for initialization
+	void Start () {
+		currentPoint = 0;
+		transform.position = patrolPoints[0].position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position == patrolPoints [currentPoint].position) {
+			currentPoint++;
+		}
+
+		if (currentPoint >= patrolPoints.Length) {
+			currentPoint = 0;
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, patrolPoints[currentPoint].position, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	//private int currentLevel;
+	public int score = 0;
+
+	//public float currentTime = 15.0f;
+	public GameObject player;
+	//public GameObject mainCanvas;
+	//public Text mainTimeDisplay;
+	//public Text mainScoreDisplay;
+	//public GameObject gameOverCanvas;
+
+	public static GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		if (gm == null) {
+			gm = GetComponent<GameManager>();
+		}
+
+		if (player == null) {
+			player = GameObject.FindGameObjectWithTag("Player");
+		}
+
+		//mainCanvas.SetActive (true);
+		//gameOverCanvas.SetActive (false);
+
+		//mainTimeDisplay.text = currentTime.ToString ("0.0");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (currentTime);
+		//currentTime -= Time.deltaTime;
+		//mainTimeDisplay.text = currentTime.ToString ("0.0");
+
+		/*if (currentTime <= 0) {
+			mainTimeDisplay.text = "0.0";
+			gameOverCanvas.SetActive(true);
+			player.SetActive(false);
+		}*/
+	}
+
+	public void CollectCake (int value){
+		score += value;
+		//mainScoreDisplay.text = score.ToString();
+		Debug.Log (score);
+	}
+	/*
+	public void CompleteLevel (){
+		Application.LoadLevel (++currentLevel);
+	}*/
+}
