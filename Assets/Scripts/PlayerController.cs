58fe9567a5aee32ee955005285b75533ac4c2ed5
Ivan Medić <medic.ivan.hr@gmail.com>
2017-06-13 14:33:11 +0200

    Various fixes added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,95 +1,147 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerController : MonoBehaviour
+//{
+//    public float speed = 10.0F;
+//    public float jumpSpeed = 0.0F;
+//    public float gravity = 20.0F;
+//    private Vector3 moveDirection = Vector3.zero;
+//    private float turner;
+//    public float sensitivity = 5.0F;
+//    static Animator anim;
+//    private CharacterController controller;
+
+
+//    void Start()
+//    {
+//        anim = GetComponent<Animator>();
+//    }
+
+//    void Update()
+//    {
+//        controller = GetComponent<CharacterController>();
+//        if (controller.isGrounded)
+//        {
+//            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+//            moveDirection = transform.TransformDirection(moveDirection);
+//            moveDirection *= speed;
+//            if (Input.GetButton("Jump"))
+//                moveDirection.y = jumpSpeed;
+
+//        }
+//        turner = Input.GetAxis("Horizontal") * sensitivity;
+//        if (turner != 0)
+//        {
+//            transform.eulerAngles += new Vector3(0, turner, 0);
+//        }
+
+//        anim.SetBool("IsWalking", Input.GetAxis("Vertical") != 0);
+
+//        moveDirection.y -= gravity * Time.deltaTime;
+//        controller.Move(moveDirection * Time.deltaTime);
+//    }
+
+
+//    void OnTriggerEnter(Collider other)
+//    {
+//        Debug.Log(other);
+//        if (other.tag == "Cookie")
+//        {
+//            Debug.Log("OnTriggerEnter() was called");
+//            Destroy(other.gameObject);
+//        }
+//    }
+
+//}
+
 using UnityEngine;
+using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-	public float speed = 10.0F;
-	public float jumpSpeed = 0.0F; 
-	public float gravity = 20.0F;
-	private Vector3 moveDirection = Vector3.zero;
-	private float turner;
-	//private float looker;
-	public float sensitivity = 5.0F;
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 
-	//public GameObject deathParticles;
-	//private Vector3 origin;
-
-	// Use this for initialization
-	void Start () {
-		//gameObject.SetActive (true);
-		//origin = transform.position;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		CharacterController controller = GetComponent<CharacterController>();
-		// is the controller on the ground?
-		if (controller.isGrounded) {
-			//Feed moveDirection with input.
-			//moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			//Multiply it by speed.
-			moveDirection *= speed;
-			//Jumping
-			if (Input.GetButton("Jump"))
-				moveDirection.y = jumpSpeed;
-
-		}
-		//turner = Input.GetAxis ("Mouse X")* sensitivity;
-		turner = Input.GetAxis ("Horizontal")* sensitivity;
-		//looker = -Input.GetAxis ("Mouse Y")* sensitivity;
-		//looker = -Input.GetAxis ("Vertical")* sensitivity;
-		if(turner != 0){
-			//Code for action on mouse moving right
-			transform.eulerAngles += new Vector3 (0,turner,0);
-		}
-		/*if(looker != 0){
-			//Code for action on mouse moving right
-			transform.eulerAngles += new Vector3 (looker,0,0);
-			//transform.eulerAngles += new Vector3 (0,looker,0);
-		}*/
-		//Applying gravity to the controller
-		moveDirection.y -= gravity * Time.deltaTime;
-		//Making the character move
-		controller.Move(moveDirection * Time.deltaTime);
-	}
-
-	/*void OnCollisionEnter(Collision other) {
-		if (other.gameObject.tag == "Enemy") {
-			Instantiate (deathParticles, transform.position, Quaternion.identity);
-			transform.position = origin;
-		}
-	}
-
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "Goal") {
-			GameManager.gm.CompleteLevel();
-		}
-	}*/
-}
-
-/*
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
-    void Update()
-    {
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
 
+    public float speed = 10.0f;
+    public float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public bool canJump = true;
+    public float jumpHeight = 2.0f;
+    private bool grounded = false;
+    private Rigidbody rigidbody;
+    static Animator anim;
+    public int cookiesCollected = 0;
+    public int cookieNumber = 100;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
+        rigidbody.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (grounded)
+        {
+            // Calculate how fast we should be moving
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rigidbody.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            // Jump
+            if (canJump && Input.GetButton("Jump"))
+            {
+                rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
         }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+
+        // We apply gravity manually for more tuning control
+        rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
+        //anim.SetBool("IsWalking", Input.GetAxis("Vertical") != 0);
+
+        grounded = false;
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.tag == "Cookie")
+        {
+            cookiesCollected++;
+            transform.localScale += new Vector3(0.05F, 0.05F, 0.05F);
+            Debug.Log(cookiesCollected);
+            Destroy(other.gameObject);
+        }
+
+        if (other.tag == "Door" && cookiesCollected == cookieNumber)
+        {
+            Debug.Log("NOVI LEVEL!");
+            //Application.LoadLevel(2);
+        }
+    }
+
+    float CalculateJumpVerticalSpeed()
+    {
+        // From the jump height and gravity we deduce the upwards speed 
+        // for the character to reach at the apex.
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
     }
 }
-*/
